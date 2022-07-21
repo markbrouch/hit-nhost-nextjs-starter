33324f12c37a5fc6b77363702ec8ec0be70af409
Raul Nohea Goodness <raulg@hekili.net>
2022-07-21 10:48:30 -1000

    start trying to load BC dates

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,13 +5,15 @@ export function parseGedcomDate(dateString: string|undefined) : Date|string|unde
     let date: Date|string|undefined;
 
     let dstring = dateString;
-    let dateEpoch: number;
+    let dateEpochMilli: number;
 
     // remove ABT
-    dstring = dstring.replace('abt. ', '');
-    dstring = dstring.replace('ABT. ', '');
-    dstring = dstring.replace('ABT ', '');
-    dstring = dstring.replace('abt ', '');
+    const prefixes = [
+        'abt.', 'ABT.', 'ABT', 'abt', 'BEF', 'c.', 'ca.', 'Abt.', 
+    ];
+    prefixes.forEach((p) => {
+        dstring = dstring.replace('${p} ', '');
+    });
 
     // check for year BC
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
@@ -20,27 +22,33 @@ export function parseGedcomDate(dateString: string|undefined) : Date|string|unde
 
     console.log("myArray ", myArray);
     if(myArray && myArray.length > 0) {
-        const yearBc = 0 - parseInt( myArray[0] );
-        console.log("yearBc ", yearBc);
-        dateEpoch = new Date(0).setUTCFullYear(yearBc);
+        const yearBc = parseInt( myArray[0] );
+        let yearBcString: string|undefined;
+        if(yearBc) {
+            yearBcString = myArray[0];
+            yearBcString = yearBcString.replace(' BC', '');
+        }
+        console.log("yearBcString ", yearBcString);
+        let dateBC = new Date(-yearBc, 0, 1);
 
         // TEMP TODO  - BC will be null
-        dateEpoch = new Date(0).setUTCFullYear(0);
+        // dateEpoch = new Date(0).setUTCFullYear(0);
 
         // date = new Date(dateEpoch);
-        date = undefined;
+        // date = undefined;
 
 
         // https://github.com/mauricio/postgresql-async/issues/248
         // use 'YYYY BC' for postgresql
-        // date = `${yearBc} BC`;
+        date = `${yearBcString}-01-01 BC`;
+        dateEpochMilli = dateBC.getTime();
     }
     else {
-        dateEpoch = Date.parse(dateString);
-        date = new Date(dateEpoch);
+        dateEpochMilli = Date.parse(dateString);
+        date = new Date(dateEpochMilli);
     }
 
-    if(dateEpoch && date) {
+    if(dateEpochMilli && date) {
         console.log(`${dstring} -> ${date}`);
     }
     else {
